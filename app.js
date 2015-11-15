48449f7fe3afb582b72bc5e2f743a9869c81122d
Jack Allen <jack.allen@advent-it.co.uk>
2015-11-15 13:36:35 +0000

    milly

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
-var app = angular.module("GameOfLife", []);
-
-app.controller("MainController", ["$scope","$interval", MainController]);
-
-var makeAlive = function (x, y) {
-    return (x == 1 && y == 1) ||
-        (x == 1 && y == 3) ||
-        (x == 4 && y == 1) ||
-        (x == 5 && y == 2) ||
-        (x == 5 && y == 3) ||
-        (x == 5 && y == 4) ||
-        (x == 4 && y == 4) ||
-        (x == 3 && y == 4) ||
-        (x == 2 && y == 4);
-}
+(function(){
+    "use strict";
+    
+    angular.module("MollyCakes", []);
+})();
